c3366f24e105ead3c03905d19b5bda7fe1325dc2
Pranjal <ra-s_al_ghul@lazarus.pit>
2015-04-14 14:09:29 +0530

    Fixed populate.py

--- populate.py
+++ populate.py
@@ -10,18 +10,26 @@ def main():
     response = s.query('id:*BoschProductDetail.aspx?pid=*', rows=300)
 #     print response.len
     pid = []
+    count  =0
+    jjj = 0
     for hit in response.results:
+        pid = []
         pid.append(hit['id'].split('=')[1])
+        print pid
+        details=crawl.getpidinfo(pid)
+        if len(details) == 0:
+            count += 1
+            continue
+        jjj += 1
+        details = details[0]
+        details['category'] = getCategory(details['category'])
+        insertProduct(details)
         # pid = []
         # pid.append(hit['id'].split('=')[1])
 
         # print pid
         # details=crawl.getpidinfo(pid)
-
-    details=crawl.getpidinfo(pid)
-    for detail in details:
-        detail['category'] = getCategory(detail['category'])
-        insertProduct(detail)
+    print count,jjj
     return
 
 def getCategory(catid):
@@ -35,14 +43,19 @@ def getCategory(catid):
 def insertReview(review):
     con = db.connect('10.5.18.67','12CS30026','dual12','12CS30026');
 
+    for key in review:
+        if review[key] is None:
+            review[key] = 'Unknown'
+
+
     with con:
         cur = con.cursor()
         query = "INSERT INTO Review(pid,title, text, nick, date, sentiment_score) VALUES("\
-                + '"' + review['pid']    + '", '\
-                + '"' + review['title']         + '", '\
-                + '"' + review['text']         + '", '\
-                + '"' + review['nick']         + '", '\
-                + '"' + review['date']         + '", '\
+                + '"' + review['pid'].replace('"',"&quot;")    + '", '\
+                + '"' + review['title'].replace('"',"&quot;")         + '", '\
+                + '"' + review['text'].replace('"',"&quot;")         + '", '\
+                + '"' + review['nick'].replace('"',"&quot;")         + '", '\
+                + '"' + review['date'].replace('"',"&quot;")         + '", '\
                 + " NULL "\
                 + ")";\
         print query
@@ -52,15 +65,20 @@ def insertReview(review):
 def insertProduct(product):
     con = db.connect('10.5.18.67', '12CS30026','dual12','12CS30026');
 
+
+    for key in product:
+        if product[key] is None:
+            product[key] = 'Unknown'
+
     with con:
         cur = con.cursor()
         query = "INSERT INTO Product(pid,pname ,plink ,rating ,description ,category) VALUES("\
-                +'"'+ product['pid']           + '", '\
-                +'"'+ product['pname']             + '", '\
-                +'"'+ product['plink']             + '", '\
-                +'"'+ product['rating']         + '", '\
-                +'"'+ product['description'] + '", '\
-                +'"'+ product['category']         + '"'\
+                +'"'+ product['pid'].replace('"',"&quot;")           + '", '\
+                +'"'+ product['pname'].replace('"',"&quot;")             + '", '\
+                +'"'+ product['plink'].replace('"',"&quot;")             + '", '\
+                +'"'+ product['rating'].replace('"',"&quot;")         + '", '\
+                +'"'+ product['description'].replace('"',"&quot;") + '", '\
+                +'"'+ product['category'].replace('"',"&quot;")         + '"'\
                 + ")";\
         print query
         cur.execute(query)
