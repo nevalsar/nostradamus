fb296a0145e02e6ca8ee8749c60e25a8a4ff04fd
Pramesh Gupta <pramesh.gupta94@gmail.com>
2015-04-14 14:22:30 +0530

    slight change

--- getproductdetails.py
+++ getproductdetails.py
@@ -1,7 +1,7 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 import MySQLdb as mdb
-db = mdb.connect('10.5.18.67','12CS30026','dual12','12CS30026')
+db = mdb.connect('10.5.18.67', '12CS30026', 'dual12', '12CS30026')
 cursor = db.cursor()
 
 pid = raw_input()
